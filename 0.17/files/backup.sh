e2bcc248e2e8d733e6e7a3c8a65a6fd0d681a9ff
Wolf Mathes <fanatik@gmx.at>
2019-09-07 13:52:03 +0700

    Initial after-fork commit

---
+++ 0.17/files/backup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+C=${1:-${FDK_DEFAULT_RCLONE_CONFIG}}
+X=RCLONE_CONFIG_${C}_REMOTE_PATH
+TARGET="${C}:${!X}"
+printf "\n\e[1;34m%-6s\e[m\n" "Storing /opt/factorio as ${TARGET}"
+mkdir -p /opt/factorio
+rclone copy -P --create-empty-src-dirs /opt/factorio "${TARGET}" -P --create-empty-src-dirs
