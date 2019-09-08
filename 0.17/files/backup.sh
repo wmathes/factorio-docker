8c7fe0d39135066e80bab38f276843c82e2d0781
Wolf Mathes <fanatik@gmx.at>
2019-09-08 14:21:59 +0700

    WIP

--- 0.17/files/backup.sh
+++ 0.17/files/backup.sh
@@ -5,9 +5,9 @@ TARGET="${C}:${!X}"
 
 printf "\n\e[1;34m%-6s\e[m\n" "Storing /opt/factorio as ${TARGET}"
 mkdir -p /opt/factorio
-rclone copy -P --create-empty-src-dirs ${CONFIG} "${TARGET}/config"
-rclone copy -P --create-empty-src-dirs ${SAVES} "${TARGET}/saves"
-rclone copy -P --create-empty-src-dirs ${MODS} "${TARGET}/mods"
+rclone copy -P --create-empty-src-dirs "${CONFIG}" "${TARGET}/config"
+rclone copy -P --create-empty-src-dirs "${SAVES}" "${TARGET}/saves"
+rclone copy -P --create-empty-src-dirs "${MODS}" "${TARGET}/mods"
 
 printf "\n\e[1;34m%-6s\e[m\n" "Stored saves:"
 ls -w 1 ${SAVES}
